cb470c21d004c613af76a1a7173a2d206bfbce72
Martin Phillips <46678229+DuckyBoss@users.noreply.github.com>
2022-01-27 16:50:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ async def ListAll(ctx):
             await ctx.send(Tech_Questions[x]["Answer"])
             await ctx.send(".\n\n.")
 
-@client.command
+@client.command()
 async def whoami(ctx):
     host_name = socket.gethostname()
     local_ip = socket.gethostbyname(hostname)
